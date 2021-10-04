f283259bee38bde0a5e220d2ed753678f22419a2
Dominik Hirt <dominik@hub28.de>
2021-10-04 13:02:08 +0200

    start persistence

--- src/main/java/de/workshops/bookdemo/book/Book.java
+++ src/main/java/de/workshops/bookdemo/book/Book.java
@@ -1,16 +1,30 @@
 package de.workshops.bookdemo.book;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "book")
 public class Book {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
     private String title;
     private String description;
     private String author;
